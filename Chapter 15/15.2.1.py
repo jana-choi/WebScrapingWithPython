e7afd4b6d8c87a9d58efd2dc984e27d40b3fd8bb
jana-choi <lovej0918@gmail.com>
2020-06-01 16:22:10 +0900

    Chapter 15

---
+++ Chapter 15/15.2.1.py
@@ -0,0 +1,22 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import unittest
+
+class TestWikipedia(unittest.TestCase):
+    bs = None
+
+    def setUpClass():
+        url = "http://en.wikipedia.org/wiki/Monty_Python"
+        TestWikipedia.bs = BeautifulSoup(urlopen(url), "html.parser")
+    
+    def test_titleText(self):
+        pageTitle = TestWikipedia.bs.find("h1").get_text()
+        self.assertEqual("Monty Python", pageTitle)
+    
+    def test_contentExists(self):
+        content = TestWikipedia.bs.find("div",{"id":"mw-content-text"})
+        self.assertIsNotNone(content)
+
+
+if __name__ == "__main__":
+    unittest.main()
